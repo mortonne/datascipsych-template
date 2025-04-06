1a6d9c8356e0d971e2251e0d84d811937de770de
Neal Morton <mortonne@gmail.com>
2025-04-06 16:17:51 -0500

    feat: function to load data dictionary

--- src/project/task.py
+++ src/project/task.py
@@ -1,7 +1,16 @@
 from importlib import resources
+import json
 
 
 def get_data_file():
     """Get path to the data file."""
     data_file = resources.files("project").joinpath("data/2011_Polyn_scored.csv")
     return data_file
+
+
+def load_data_dict():
+    """Read data dictionary for the dataset."""
+    dict_file = resources.files("project").joinpath("data/data_dictionary.json")
+    with open(dict_file) as f:
+        d = json.load(f)
+    return d
