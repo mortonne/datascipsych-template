f8cddceef7e4d32a2062f9105d6777f57d5393bb
Neal Morton <mortonne@gmail.com>
2025-04-06 15:58:15 -0500

    refactor: move data to subdirectory

--- src/project/task.py
+++ src/project/task.py
@@ -3,5 +3,5 @@ from importlib import resources
 
 def get_data_file():
     """Get path to the data file."""
-    data_file = resources.files("project").joinpath("2011_Polyn_scored.csv")
+    data_file = resources.files("project").joinpath("data/2011_Polyn_scored.csv")
     return data_file
